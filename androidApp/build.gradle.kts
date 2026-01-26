23be5437478d74c9b8733cec65754f694f3f8f46
ericscheier <eric@scheier.org>
2026-01-25 20:37:06 -0500

    chore: bump version to 2.1.1 - redeploy with Grid features

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         applicationId = "eco.emergi.embit"
         minSdk = 24
         targetSdk = 35
-        versionCode = 3
-        versionName = "2.1.0"
+        versionCode = 4
+        versionName = "2.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
