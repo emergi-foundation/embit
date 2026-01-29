b59a9b2322daa670d9476b5407cff2a526cd3869
ericscheier <eric@scheier.org>
2026-01-29 12:19:34 -0500

    chore: bump version to 2.2.2

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         applicationId = "eco.emergi.embit"
         minSdk = 24
         targetSdk = 35
-        versionCode = 14
-        versionName = "2.2.1"
+        versionCode = 15
+        versionName = "2.2.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
