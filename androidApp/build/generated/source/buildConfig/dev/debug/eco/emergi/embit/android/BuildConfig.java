55771233e25ae2af20a18e5133a22622d8d5c137
ericscheier <eric@scheier.org>
2026-01-28 23:44:04 -0500

    docs: Update documentation for simplified workflow

--- androidApp/build/generated/source/buildConfig/dev/debug/eco/emergi/embit/android/BuildConfig.java
+++ androidApp/build/generated/source/buildConfig/dev/debug/eco/emergi/embit/android/BuildConfig.java
@@ -8,8 +8,8 @@ public final class BuildConfig {
   public static final String APPLICATION_ID = "eco.emergi.embit";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "dev";
-  public static final int VERSION_CODE = 7;
-  public static final String VERSION_NAME = "2.1.4-dev";
+  public static final int VERSION_CODE = 14;
+  public static final String VERSION_NAME = "2.2.1-dev";
   // Field from product flavor: dev
   public static final String API_BASE_URL = "https://dev-api.embit.eco";
   // Field from product flavor: dev
